82ba603315143cd452c534c57935ad5c5b846199
tsach <tsachbak9@gmail.com>
2026-02-13 12:14:27 +0200

    Add ObjectsController to handle API request for Map Objects.

---
+++ server/server/Controllers/ObjectsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.Dtos.Objects;
+
+namespace server.Controllers
+{
+    /// <summary>
+    /// ObjectsController handles API requests related to Map Objects.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ObjectsController : ControllerBase
+    {
+        [HttpPost("save")]
+        public IActionResult Save([FromBody] SaveObjectsRequestDto request)
+        {
+            var response = new SaveObjectsResponseDto
+            {
+                Count = request.Objects.Count,
+                Objects = request.Objects
+            };
+
+            return Ok(response);
+        }
+    }
+}
